e80e131581afef7273b26045cf4c5877e88ff188
Bill Burgess <wmhburgess@gmail.com>
2016-11-04 12:30:16 +1300

    Changed some commas to semicolons, game now works

--- minesweeper.js
+++ minesweeper.js
@@ -7,7 +7,6 @@ function Cell(row, col, isMine){
   this.col = col;
   this.isMine = isMine;
   this.hidden = true;
-  this.
 }
 var cell0 = new Cell(0, 0, false)
 var cell1 = new Cell(1, 0, false)
@@ -23,12 +22,13 @@ board.cells = [cell0, cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8,]
 
 function startGame () {
   var cellNum = board.cells.length;
-  for (var i = 0, i < cellNum, i++){
+  for (var i = 0; i < cellNum; i++){
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
   // Don't remove this function call: it makes the game work!
   lib.initBoard();
 }
+
 
 // Define this function to look for a win condition:
 //
@@ -37,7 +37,7 @@ function startGame () {
 function checkForWin () {
   document.addEventListener('click', checkForWin);
   document.addEventListener('contextmenu', checkForWin);
-  for(var i = 0, i < board.cells.length, i++){
+  for(var i = 0; i < board.cells.length; i++){
     if(board.cells[i].isMine && !board.cells[i].isMarked){
       return;
     }else if(board.cells[i].isMine && !board.cells[i].hidden){
@@ -50,18 +50,19 @@ function checkForWin () {
   //   lib.displayMessage('You win!')
 }
 
+
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
 // cells yourself! Just use `lib.getSurroundingCells`:
 //
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+//var surrounding = lib.getSurroundingCells(cell.row, cell.col)
 //
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
   var surrounding = lib.getSurroundingCells(cell.row, cell.col);
   var count = 0;
-  for (var i = 0, i < surrounding.length, i++){
+  for (var i = 0; i < surrounding.length; i++){
     if(surrounding[i].isMine){
       count++;
     }
